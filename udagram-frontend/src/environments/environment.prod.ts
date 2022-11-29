3c1c0b406b5510e2b2908f3e2ddcb46cbf3cd89f
lequangtin <lequangtin95@gmail.com>
2022-11-29 11:27:00 +0700

    add reserverproxy external IP

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: 'Our Udagram Microservices App',
+  apiHost: 'http://ab1165584aa9846f79a2873bf55b1caf-160180765.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
